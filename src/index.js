d11a70eee4e8b96b23b1c9913bd1d8c1fea1f15c
LeonardoCHb <leonardohably@hotmail.com>
2021-05-29 10:50:17 -0300

    Challenge Done

--- src/index.js
+++ src/index.js
@@ -23,24 +23,28 @@ app.post("/repositories", (request, response) => {
     likes: 0
   };
 
-  return response.json(repository);
+  repositories.push(repository)
+
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
   const updatedRepository = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
-  const repository = { ...repositories[repositoryIndex], ...updatedRepository };
+  if (updatedRepository.likes) {
+    delete updatedRepository.likes
+  }
 
-  repositories[repositoryIndex] = repository;
+  newRepository = Object.assign(repositories[repositoryIndex], updatedRepository);
 
-  return response.json(repository);
+  return response.json(newRepository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
@@ -48,7 +52,7 @@ app.delete("/repositories/:id", (request, response) => {
 
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
@@ -66,9 +70,11 @@ app.post("/repositories/:id/like", (request, response) => {
     return response.status(404).json({ error: "Repository not found" });
   }
 
-  const likes = ++repositories[repositoryIndex].likes;
+  repositories[repositoryIndex].likes += 1;
 
-  return response.json('likes');
+  likes = repositories[repositoryIndex].likes
+
+  return response.json({ likes });
 });
 
 module.exports = app;
